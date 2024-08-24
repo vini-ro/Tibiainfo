8a95e2be5f76aea4200bdb23d7e94b5871ed5f64
Vinicius <vroliveira.eng@gmail.com>
2024-08-24 17:19:45 -0300

    Initial Commit

---
+++ Tibiainfo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tibiainfo
+//
+//  Created by Vinicius Oliveira on 24/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tibiainfo/TibiainfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TibiainfoApp.swift
+//  Tibiainfo
+//
+//  Created by Vinicius Oliveira on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TibiainfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
